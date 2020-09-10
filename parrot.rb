9fe8a4ad8a5beaee3d1ed5190154ec2af23ccfb6
Nattida Wong <nattidac.wong@gmail.com>
2020-09-10 18:38:55 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+def parrot
+  "Squawk!"
+end
+
+def parrot(phrase="Squawk!")
+  puts "#{phrase}"
+  "Pretty bird!"
+end
+
